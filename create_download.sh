468fddfe3bd8789bd50f42324a74412b9f445d52
b0wter <b0wter@gmx.de>
2018-12-27 21:52:10 +0100

    Updated shell scripts to create downloads. Also added a sample of a convenience script that makes creating downlaods easier.

--- create_download.sh
+++ create_download.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ge 3 ]; then
-	rm $2
+if ! [ -x "$(command -v 7z)" ]; then
+	echo "7zip is not installed."
+	exit 1
+fi
+
+if [ "$#" -ge 4 ]; then
+	rm $3
 	TOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 	PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-	7z a -p$PASSWORD -mhe=on $2 "${@:3}"
-	cp $2 $1
-	TOKEN_FILE=$(basename -- "$2" ".7z")
-	echo "$TOKEN" >> "$1/${TOKEN_FILE}.token"
+	7z a -p$PASSWORD -mhe=on $3 "${@:4}"
+	cp $3 $2
+	TOKEN_FILE=$(basename -- "$3" ".7z")
+	echo "$TOKEN" >> "$2/${TOKEN_FILE}.token"
 
 	echo "The following password is NOT recoverable! Please write them down carefully."
 	echo "7z password:    $PASSWORD"
-	echo "The download token was written to $1/${TOKEN_FILE}.token."
+	echo "The download token was written to $2/${TOKEN_FILE}.token."
 	echo "Download token: $TOKEN"
+	echo "Download url:"
+	echo "$1?filename=$3&token=$TOKEN"
 else
 	echo "You ned to supply at least three parameters to this script:"
-       	echo "1. folder for the download"
-	echo "2. name of the download"
-	echo "3. an arbitrary number of files that will be used as the contents of the new zip"
+	echo "1. the base url of your torpedo instance"
+       	echo "2. folder for the download"
+	echo "3. name of the download"
+	echo "4. an arbitrary number of files that will be used as the contents of the new zip, must be parseable by 7zip"
 fi
