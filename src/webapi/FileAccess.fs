b33ea0ef988fad42943a0f92014ba311c55f6df5
b0wter <b0wter@gmx.de>
2019-01-03 09:41:21 +0100

    Made FileAccess module more testable be injecting dependencies.

--- src/webapi/FileAccess.fs
+++ src/webapi/FileAccess.fs
@@ -14,58 +14,82 @@ let tokenExtension = ".token"
 /// Only considers those Token files which have exactly one corresponding content file.
 /// (e.g.: cook.png cook.bmp & cook.token would NOT be returned but shark.png & shark.token would be)
 /// </summary>
-let getFilesWithTokens (folder: string): DownloadPair[] =
-    Directory.GetFiles(folder)
-    |> Array.map Path.GetFileName
-    |> Array.groupBy (fun name -> Path.GetFileNameWithoutExtension(name))
-    |> Array.map     (fun (name,  fullnames)  -> (name, fullnames |> Array.map Path.GetExtension))
+let customGetFilesWithTokens (getFilesInFolder: string -> string [])
+                             (getFileNameWithoutExtension: string -> string) 
+                             (getFileName: string -> string) 
+                             (getFileExtension: string -> string) 
+                             (pathCombinator: (string * string) -> string)
+                             (folder: string): DownloadPair[] =
+    getFilesInFolder(folder)
+    |> Array.map getFileName
+    |> Array.groupBy (fun name -> getFileNameWithoutExtension(name))
+    |> Array.map     (fun (name,  fullnames)  -> (name, fullnames |> Array.map getFileExtension))
     |> Array.filter  (fun (_,     extensions) -> extensions |> Array.exists (fun extension -> extension.EndsWith(tokenExtension)))
     |> Array.filter  (fun (_,     extensions) -> extensions.Length = 2)
-    |> Array.map     (fun (name,  extensions) -> (Path.Combine(folder, name + extensions.[0]), (Path.Combine(folder, name + extensions.[1]))))
+    |> Array.map     (fun (name,  extensions) -> (pathCombinator(folder, name + extensions.[0]), (pathCombinator(folder, name + extensions.[1]))))
     |> Array.map     (fun (first, second)     -> if first.EndsWith(tokenExtension) then (first, second) else (second, first))
+    
+let getFilesWithTokens =
+    customGetFilesWithTokens Directory.GetFiles
+                             Path.GetFileNameWithoutExtension
+                             Path.GetFileName
+                             Path.GetExtension
+                             Path.Combine
     
 /// <summar>
 /// Reads text contents from a file. 
 /// Will throw exceptions if the file is unknown or the contents
 /// cannot be read.
 /// </summary>
-let getTextContent (filename: string) =
-    File.ReadAllText(filename)    
+let customGetTextContent (textFileReader: string -> string) (filename: string) =
+    textFileReader(filename)    
+    
+let getTextContent =
+    customGetTextContent File.ReadAllText
     
 /// <summary>
 /// Get the timestamp of the last mofication (local system time).
 /// Will throw an exception if the file does not exist.
 /// </summary>    
-let getLastModified (filename: string) =
-    File.GetLastWriteTime(filename)    
-    
+let customGetLastModified (lastWriteTimeProvider: string -> DateTime) (filename: string) =
+    lastWriteTimeProvider(filename)    
+        
+let getLastModified =        
+    customGetLastModified File.GetLastWriteTime
+
 /// <summary>
 /// Retrieves the last modification dates for a list of files.
 /// Will throw an exception if any of the files does not exist.
 /// </summary>    
 let getFileDates (filenames: string seq) =
     filenames
-    |> Seq.map File.GetLastWriteTime    
+    |> Seq.map getLastModified
     
 /// <summary>
 /// Checks if the given file is downloadable meaning a matching Token file exists
 /// and the Token is uniquely bound to a single content file.
 /// </summary>
-let existsIn (folder: string) (filename: string): bool =
+let customExistsIn (pathCombinator: (string * string) -> string) (folder: string) (filename: string): bool =
     if folder.Contains("..") || filename.Contains("..") then 
         false 
     else
         getFilesWithTokens folder |> Array.map snd |> Array.contains (Path.Combine(folder, filename))
     
+let existsIn =
+    customExistsIn Path.Combine
+    
 /// <summary>
 /// Interprets the filename as a combination of directory and filename and checks if it exists.
 /// Will always return false if there is a ".." in the filename.
 /// </summary>    
-let fileExists (filename: string) : bool =
+let customFileExists (fileExistenceChecker: string -> bool) (filename: string) : bool =
     if filename.Contains("..") then
         false 
     else
-        File.Exists filename
+        fileExistenceChecker filename
+    
+let fileExists =
+    customFileExists File.Exists 
     
 /// <summary>
 /// Tries to open a file stream for the given file. 
@@ -87,5 +111,8 @@ let fileStream (filename: string): FileStream option =
 /// <summary>
 /// Writes the given content into the given file using the default character encoding.
 /// </summary>
-let persistStringAsFile (filename: string) (content: string) =
-    File.WriteAllText(filename, content)
+let customPersistStringAsFile (persistor: (string * string) -> unit) (filename: string) (content: string) =
+    persistor(filename, content)
+    
+let persistStringAsFile =
+    customPersistStringAsFile File.WriteAllText 
